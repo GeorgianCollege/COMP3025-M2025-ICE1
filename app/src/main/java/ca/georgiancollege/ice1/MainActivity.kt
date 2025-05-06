9e7727813384ed35c85a527f9f0fe837d76bc22d
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2025-05-06 16:42:06 -0400

    added button press functionality

--- app/src/main/java/ca/georgiancollege/ice1/MainActivity.kt
+++ app/src/main/java/ca/georgiancollege/ice1/MainActivity.kt
@@ -1,13 +1,18 @@
 package ca.georgiancollege.ice1
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class MainActivity : AppCompatActivity()
+{
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
@@ -15,6 +20,14 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+
+        val greetingTextView = findViewById<TextView>(R.id.greetingTextView)
+        val greetingButton = findViewById<Button>(R.id.greetingButton)
+
+        greetingButton.setOnClickListener {
+            greetingTextView.text = "Hello, World!"
         }
     }
 }
